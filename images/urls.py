5ff392723b16c8f9cb66a06f7e829c48a904c442
Amin <asaa47721@gmail.com>
2020-02-27 03:12:33 +0330

    add image_detail view to urls

--- images/urls.py
+++ images/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'images'
 
 urlpatterns = [
+    # create image
     path('create/', views.image_create, name='create'),
+
+    # image detail
+    path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
 ]
