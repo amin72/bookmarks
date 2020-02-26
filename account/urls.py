438a1d46373db46f191e20b242134900e9796f97
Amin <asaa47721@gmail.com>
2020-02-26 03:44:14 +0330

    add password change urls to account app

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -14,4 +14,13 @@ urlpatterns = [
 
     # dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    # change password urls
+    path('password_change/',
+        auth_views.PasswordChangeView.as_view(
+            success_url = reverse_lazy('account:password_change_done')),
+        name='password_change'),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
 ]
