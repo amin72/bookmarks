beb9a8eb9d2ef94487a5786bc4d0c9206a767af4
Amin <asaa47721@gmail.com>
2020-02-26 19:16:53 +0330

    define edit view to update user and profile data

--- account/views.py
+++ account/views.py
@@ -39,3 +39,27 @@ def register(request):
         'user_form': user_form
     }
     return render(request, 'account/register.html', context)
+
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+    user_form = UserEditForm(instance=request.user)
+    profile_form = ProfileEditForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+
+    return render(request, 'account/edit.html', context)
