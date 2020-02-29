c1282f7d24e72372799f2ffcf8b63040e1b3b0a0
Amin <asaa47721@gmail.com>
2020-02-29 14:15:51 +0330

    fix mistakes in create_action utility

--- actions/utils.py
+++ actions/utils.py
@@ -1,4 +1,6 @@
+import datetime
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 from .models import Action
 
 
@@ -8,16 +10,16 @@ def create_action(user, verb, target=None):
     If action exist in database and was created within last minute False will
     be returned otherwise True
     """
-    
+
     # check for any similar action made in the last minute
     now = timezone.now()
     last_minute = now - datetime.timedelta(seconds=60)
     similar_actions = Action.objects.filter(user_id=user.id,
         verb=verb,
-        created_gte=last_minute)
+        created__gte=last_minute)
 
     if target:
-        target_ct = ContentType.objects.get_for_model(taget)
+        target_ct = ContentType.objects.get_for_model(target)
         similar_actions = similar_actions.filter(target_ct=target_ct,
             target_id=target.id)
 
