41e1c3576a296cc407f2fa88e078710bc4121c6e
Amin <asaa47721@gmail.com>
2020-02-29 12:42:11 +0330

    define create_action utility

---
+++ actions/utils.py
@@ -0,0 +1,28 @@
+from django.contrib.contenttypes.models import ContentType
+from .models import Action
+
+
+def create_action(user, verb, target=None):
+    """
+    Try to create an action instance for given info.
+    If action exist in database and was created within last minute False will
+    be returned otherwise True
+    """
+    
+    # check for any similar action made in the last minute
+    now = timezone.now()
+    last_minute = now - datetime.timedelta(seconds=60)
+    similar_actions = Action.objects.filter(user_id=user.id,
+        verb=verb,
+        created_gte=last_minute)
+
+    if target:
+        target_ct = ContentType.objects.get_for_model(taget)
+        similar_actions = similar_actions.filter(target_ct=target_ct,
+            target_id=target.id)
+
+    if not similar_actions:
+        # no existing actions found
+        Action.objects.create(user=user, verb=verb, target=target)
+        return True
+    return False
